14f1f1cfb62d90b3b959d20293ec497e3b674ad7
Sebastian Alfonso Huacasi <aalfonso@unsa.edu.pe>
2025-12-19 00:57:05 -0500

    Subiendo slides de inciio y fin de mis niveles

--- TrabajoFinal/Assets/Scripts/GeneradorGinyuh.cs
+++ TrabajoFinal/Assets/Scripts/GeneradorGinyuh.cs
@@ -6,27 +6,32 @@ public class GeneradorEnemigos : MonoBehaviour
     public Transform goku;
 
     // CONFIGURACION DE SPAWN
-    public float tiempoEntreSpawns = 4f;
-    public float distanciaMinima = 20f; // Que tan lejos aparece (20 metros)
-    public float dispersion = 10f;      // Que tan "desordenados" aparecen
+    [Header("--- Tiempos ---")]
+    public float tiempoEsperaInicial = 10f; // <--- NUEVO: Cuánto tarda en salir el PRIMERO
+    public float tiempoEntreSpawns = 4f;    // Cada cuánto salen después
+
+    [Header("--- Posición ---")]
+    public float distanciaMinima = 20f;
+    public float dispersion = 10f;
 
     void Start()
     {
-        InvokeRepeating("CrearEnemigo", 2f, tiempoEntreSpawns);
+        // El primer número (tiempoEsperaInicial) es cuánto espera antes de empezar.
+        // El segundo (tiempoEntreSpawns) es el ritmo normal.
+        InvokeRepeating("CrearEnemigo", tiempoEsperaInicial, tiempoEntreSpawns);
     }
 
     void CrearEnemigo()
     {
         if (goku == null) return;
 
-        // MATEMATICA PARA APARECER ENFRENTE
-        // 1. Tomamos la posición 20 metros ADELANTE de Goku (goku.forward)
+        // 1. Tomamos la posición 20 metros ADELANTE de Goku
         Vector3 puntoFrente = goku.position + (goku.forward * distanciaMinima);
 
-        // 2. Le sumamos un valor aleatorio (izquierda/derecha/arriba/abajo) para que no salgan en fila india
+        // 2. Le sumamos aleatoriedad
         Vector3 posicionFinal = puntoFrente + (Random.insideUnitSphere * dispersion);
 
-        // 3. Forzamos que la altura (Y) sea parecida a la de Goku para que no salgan bajo tierra o en el espacio
+        // 3. Ajustamos altura
         posicionFinal.y = goku.position.y + Random.Range(-2f, 5f);
 
         // 4. Crear al Ginyu
